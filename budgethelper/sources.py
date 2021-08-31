01711f67a506e9131f46720d558a0d7be1e0f048
Preocts <preocts@gmail.com>
2021-08-30 22:39:55 -0400

    Draft out CRUD operations for sources

--- budgethelper/sources.py
+++ budgethelper/sources.py
@@ -4,9 +4,12 @@ Class object for handling sources
 Author: Preocts
 """
 import logging
+from typing import List
 
 from budgethelper.dbconnection import DBConnection
+from budgethelper.exceptions import SourceTableError
 from budgethelper.models.database import Database
+from budgethelper.models.source import SourceRow
 
 
 class DBSources(DBConnection):
@@ -26,3 +29,86 @@ class DBSources(DBConnection):
         """
         database_name = Database.name
         super().__init__(database_name)
+
+    def create(self, sourcerow: SourceRow) -> None:
+        """Creates a source row in the database"""
+
+        sql = "INSERT INTO sources (name, created_on, updated_on) VALUES (?, ?, ?)"
+        values = (sourcerow.name, sourcerow.created_on, sourcerow.updated_on)
+
+        cursor = self.conn.cursor()
+
+        try:
+            cursor.execute(sql, values)
+            self.conn.commit()
+
+        finally:
+            cursor.close()
+
+    def read(self, uid: int) -> SourceRow:
+        """Return source by uid"""
+
+        sql = "SELECT name, created_on, updated_on, uid FROM sources WHERE uid = ?"
+        values = (uid,)
+
+        cursor = self.conn.cursor()
+
+        try:
+            cursor.execute(sql, values)
+            result = cursor.fetchone()
+
+        finally:
+            cursor.close()
+
+        if not result:
+            msg = f"Unable to read, UID not found: {uid}"
+            self.log.error(msg)
+            raise SourceTableError(msg)
+
+        return SourceRow(*result)
+
+    def getlist(self) -> List[SourceRow]:
+        """Get all sources from database"""
+
+        sql = "SELECT name, created_on, updated_on, uid FROM sources"
+
+        cursor = self.conn.cursor()
+
+        try:
+            cursor.execute(sql)
+            results = cursor.fetchall()
+
+        finally:
+            cursor.close()
+
+        return [SourceRow(*row) for row in results]
+
+    def update(self, source: SourceRow) -> None:
+        """Update a source in the database"""
+
+        sql = "UPDATE sources SET name = ?, updated_on = ? WHERE uid = ?"
+        values = (source.name, source.updated_on, source.uid)
+
+        cursor = self.conn.cursor()
+
+        try:
+            cursor.execute(sql, values)
+            self.conn.commit()
+
+        finally:
+            cursor.close()
+
+    def delete(self, uid: int) -> None:
+        """Delete a row out of the database, may have downstream impact"""
+
+        sql = "DELETE from sources WHERE uid = ?"
+        values = (uid,)
+
+        cursor = self.conn.cursor()
+
+        try:
+            cursor.execute(sql, values)
+            self.conn.commit()
+
+        finally:
+            cursor.close()
